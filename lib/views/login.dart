b9395d04426198cf2ef146a05425d9408ff2ddc8
Emison <emisondias1@gmail.com>
2020-09-22 00:17:02 -0300

    adicionado pagina de login paara ineração com firebase

---
+++ lib/views/login.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+class LoginPgae extends StatefulWidget {
+  @override
+  _LoginPgaeState createState() => _LoginPgaeState();
+}
+
+class _LoginPgaeState extends State<LoginPgae> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

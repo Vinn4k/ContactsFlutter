456c6e38b7ded8e1afa8a3d11e98727a70729af8
Emison <emisondias1@gmail.com>
2020-10-05 22:33:24 -0300

    ta retornando nullo o usuario

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:alltolkit/views/login.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'views/home_page.dart';
 
 void main() {
+
   runApp(MaterialApp(
+
     home: LoginPgae(),
     debugShowCheckedModeBanner: false,
 
--- lib/views/login.dart
+++ lib/views/login.dart
@@ -1,68 +1,109 @@
 import 'package:alltolkit/views/home_page.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 class LoginPgae extends StatefulWidget {
   @override
   _LoginPgaeState createState() => _LoginPgaeState();
 }
 
 class _LoginPgaeState extends State<LoginPgae> {
+  final GoogleSignIn googleSignIn=GoogleSignIn();
+  FirebaseUser _currentUser;
+
+  @override
+  void initState() {
+    super.initState();
+    FirebaseAuth.instance.onAuthStateChanged.listen((user) {
+      setState(() {
+        _currentUser=user;
+      });
+
+
+    });
+
+
+  }
+
+  Future<FirebaseUser> _getuser() async{
+    if(_currentUser !=null)return _currentUser;
+    try{
+
+      final GoogleSignInAccount googleSignInAccount= await googleSignIn.signIn();
+      final GoogleSignInAuthentication googleSignInAuthentication=await googleSignInAccount.authentication;
+      final AuthCredential credential=GoogleAuthProvider.getCredential(
+        idToken: googleSignInAuthentication.idToken,
+        accessToken: googleSignInAuthentication.accessToken,);
+      final AuthResult authResult=await FirebaseAuth.instance.signInWithCredential(credential);
+      final FirebaseUser user= authResult.user;
+      return user;
+    }catch (error){
+      return null;
+
+    }
+  }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-      color: Colors.amber,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          SizedBox(
-            height: MediaQuery.of(context).size.height/8,
-          ),
-          Text("Login", style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-              fontSize: 30
-          ),),
-          SizedBox(
-            height: MediaQuery.of(context).size.height/3,
-          ),
 
-          Center(
-
-            child: Container(
-                width: 130.0,
-                height: 130.0,
-                alignment: Alignment.center,
-
-                decoration: BoxDecoration(
-                  color: Colors.red,
-                  shape: BoxShape.circle,
-
+      return Scaffold(
+          body: Container(
+            color: Colors.amber,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.height/8,
                 ),
-              child:FlatButton( child:  Text("G", style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                  fontSize: 80
-              ),
-              ),
-                onPressed:(){
-                  Navigator.pop(context);
-                  Navigator.push(context, MaterialPageRoute(builder: (context){
-                    return HomePage();
-                  }));
+                Text("Login", style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 30
+                ),),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height/3,
+                ),
 
-                },
-              )
+                Center(
+
+                  child: Container(
+                      width: 130.0,
+                      height: 130.0,
+                      alignment: Alignment.center,
+
+                      decoration: BoxDecoration(
+                        color: Colors.red,
+                        shape: BoxShape.circle,
+
+                      ),
+                      child:FlatButton( child:  Text("G", style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 80
+                      ),
+                      ),
+                        onPressed:() async{
+
+                       final  FirebaseUser user=await _getuser();
+                       print("###############");
+
+                       print(user);
+                       print("###############");
+
+                        },
+                      )
+                  ),
+
+                )
 
 
+              ],
             ),
-
           )
+      );
 
+    }
 
-        ],
-      ),
-      )
-    );
   }
-}
+
